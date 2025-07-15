8a290e487ca747d702ccebc4d2fcc8fc9936046a
fish fish <yuzhongrong2018@gmail.com>
2025-07-15 05:29:08 +0000

    为什么项目还是有127.0.0.1:21017

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,8 @@
-import {genkit} from 'genkit';
+import {genkit, noopTraceStore} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI()],
   model: 'googleai/gemini-2.0-flash',
+  traceStore: noopTraceStore, // Use in-memory trace store to avoid default mongo connection
 });
