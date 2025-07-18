ab810560dfbb6d06adcc747b3f0cf53679fcbc70
fish fish <yuzhongrong2018@gmail.com>
2025-07-18 10:02:56 +0000

    电报机器人通知少了个ca即合约地址属性

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -158,6 +158,7 @@ Token: *${pair.baseToken?.symbol || 'N/A'}*
 Action: *买入*
 RSI (1H): \`${rsi1h.toFixed(2)}\`
 RSI (5m): \`${rsi5m.toFixed(2)}\`
+CA: \`${tokenContractAddress}\`
 
 [View on GMGN](https://gmgn.ai/sol/token/${pair.pairAddress})
                         `;
